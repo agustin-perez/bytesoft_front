2456c26199686f6c8ae2f337df89263a2b714a08
agustin-perez <agperez.prieto@gmail.com>
2020-10-08 00:48:19 -0300

    aaa

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       { id: 'asfa1', ChartName: 'Tu', ChartDesc: 'Tu viejaaaaaa', xAxis: ['aaaaaaaaaaaaaaaaaaaaa', 'Sample 2', 'Sample 3'], yAxis: [300, 400, 11]},
       { id: 'asfa1', ChartName: 'Vieja', ChartDesc: 'Tu viejaaaaaa es', xAxis: ['Sample 1', 'Sample 2', 'Sample 3'], yAxis: [800, 300, 118]},
       { id: 'asfa1', ChartName: 'Es', ChartDesc: 'nooo que estás mirando', xAxis: ['Sample 1', 'Sample 2', 'Sample 3'], yAxis: [900, 600, 118]},
-      { id: 'asfa1', ChartName: '..', ChartDesc: 'Chinchulín', xAxis: ['Sample 1', 'Sample 2', 'Sample 3'], yAxis: [700, 500, 11]}
+      { id: 'asfa1', ChartName: 'Re', ChartDesc: 'Chinchulín', xAxis: ['Sample 1', 'Sample 2', 'Sample 3'], yAxis: [700, 500, 11]}
     ],
   }
 
