eecb63359a287d2c6130180fad7d7b940e8bc666
Agustín Pérez Prieto <aperez@Thinkpad-T430Deb.agp>
2020-11-04 21:24:30 -0300

    aa

--- src/Components/Navbar/LeftNav.js
+++ src/Components/Navbar/LeftNav.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import "./Navbar.css";
-import "./Navbar";
+import logo from '../../Assets/Media/logo white.png'
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import './Navbar.css';
 
 const LeftNav = ( props ) => 
 {
     return (
-        <div className="">
-
-        </div>
+          <div className="LeftNav">
+            <button className="LeftNav-responsive-buttons">&#9776;</button>
+            <Link className="LeftNav-link" to={props.root}><img className="LeftNav-mainlogo" src={logo} alt="Logo"/></Link>
+            <nav>
+              <ul>
+                  <li><Link className="LeftNav-link" to={props.root}>Inicio</Link></li>
+                  <li><Link className="LeftNav-link" to={props.about}>Sobre nosotros</Link></li>	
+                  <li><Link className="LeftNav-link" to={props.medicare}>MediCare™</Link></li>	
+                  <li><Link className="LeftNav-link" to={props.contacto}>Contacto</Link></li>	
+              </ul>
+            </nav>
+          </div>
     );
 }
 
